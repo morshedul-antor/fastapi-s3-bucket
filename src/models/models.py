1d803d65c9cdcebbb3fb11451d4b09ad9ece80ef
morshedul-antor <antor.morshedul@gmail.com>
2023-05-03 17:49:32 +0600

    add: image table

--- src/models/models.py
+++ src/models/models.py
@@ -1,8 +1,16 @@
-from sqlalchemy import Column, String, Text
+from sqlalchemy import Column, String, Text, ForeignKey, Integer
 from models import BaseModel
 
 
 class ToDo(BaseModel):
-       __tablename__ = "todo"
-       title = Column(String(255))
-       task = Column(Text, nullable=True)
+    __tablename__ = "todo"
+    title = Column(String(255))
+    task = Column(Text, nullable=True)
+
+
+class Image(BaseModel):
+    __tablename__ = "Images"
+    service_name = Column(String(255), nullable=True)
+    image_string = Column(String(255), nullable=True)
+    todo_id = Column(Integer, ForeignKey("todo.id"))
+    
