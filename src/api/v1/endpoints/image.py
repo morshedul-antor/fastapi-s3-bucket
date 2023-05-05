f32743d864718bcd877ae8de785e7cc6230068dc
morshedul-antor <antor.morshedul@gmail.com>
2023-05-05 21:25:25 +0600

    feat: image upload option null

--- src/api/v1/endpoints/image.py
+++ src/api/v1/endpoints/image.py
@@ -1,15 +1,16 @@
-from fastapi import APIRouter, Depends, UploadFile, File, Form
+from fastapi import APIRouter, Depends, UploadFile, File
 from schemas import ImageOut, TodoIn
 from services import image_service
 from db import get_db
 from exceptions import handle_result
 from sqlalchemy.orm import Session
+from typing import Optional
 
 
 router = APIRouter()
 
 
 @router.post('/', response_model=ImageOut)
-def add_image(file: UploadFile = File(...), image_data: TodoIn = Depends(),  db: Session = Depends(get_db)):
+def add_image(file: Optional[UploadFile] = File(None), image_data: TodoIn = Depends(),  db: Session = Depends(get_db)):
     create  = image_service.add_image(db=db, file=file, data_in=image_data)
     return handle_result(create)
