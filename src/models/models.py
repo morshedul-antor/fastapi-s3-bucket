bf0541ed60804d79c9be60533ab07e6d8bee30a1
morshedul-antor <antor.morshedul@gmail.com>
2023-08-29 13:21:17 +0600

    add: images table column

--- src/models/models.py
+++ src/models/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Text, ForeignKey, Integer
+from sqlalchemy import Column, String, Text, ForeignKey, Integer, Boolean
 from models import BaseModel
 
 
@@ -12,6 +12,10 @@ class Image(BaseModel):
     __tablename__ = "Images"
     service_name = Column(String(255), nullable=True)
     image_string = Column(String(255), nullable=True)
+    bucket_string = Column(String(255), nullable=True)
+    bucket_folder = Column(String(255), nullable=True)
+    image_url = Column(String(255), nullable=True)
+    bucket = Column(Boolean, nullable=True, default=False)
     todo_id = Column(Integer, ForeignKey("todo.id"))
 
 
@@ -20,4 +24,3 @@ class Picture(BaseModel):
     service_name = Column(String(255), nullable=True)
     image_string = Column(String(255), nullable=True)
     todo_id = Column(Integer, ForeignKey("todo.id"))
-    
